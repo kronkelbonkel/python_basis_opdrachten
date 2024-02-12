11ca34b7e1ceec3576cb93ef64ddc24b17ee4951
kronkelbonkel <lisanneterhaar@gmail.com>
2024-02-12 11:39:34 +0100

    lees je dit wel echt? of is dit voor saus?

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -7,3 +7,10 @@
 # Hier start de for-loop
 
 my_list = []
+
+for i in range(3, 81, 3):
+    # Voeg het gekwadrateerde en gedeelde getal toe aan de lijst
+    resultaat = (i ** 2) / 3.0
+    my_list.append(resultaat)
+
+print(my_list)
