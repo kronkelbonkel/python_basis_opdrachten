f720e7c34200f396573367bbefc87c0c934ec04f
kronkelbonkel <lisanneterhaar@gmail.com>
2024-02-12 11:37:45 +0100

    kaka dit is easyyyyyy

--- opdrachten/060_loops/opdr_1/opdr_1.py
+++ opdrachten/060_loops/opdr_1/opdr_1.py
@@ -7,3 +7,9 @@
 # Hier start de for-loop....
 
 my_list = []
+
+for i in range(1, 11):
+    my_list.append(i)
+
+# Print de lijst op het scherm
+print(my_list)
--- opdrachten/060_loops/opdr_2/opdr_2.py
+++ opdrachten/060_loops/opdr_2/opdr_2.py
@@ -8,3 +8,8 @@
 
 my_list = []
 
+for i in range(10, 51, 10):
+    my_list.append(i)
+
+print(my_list)
+
