a9ddad47a4ac9b878207c6499807d1d4686cea79
kronkelbonkel <lisanneterhaar@gmail.com>
2024-02-12 11:17:54 +0100

    oeps

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -5,8 +5,16 @@
 # Hier komt je code...
 
 gasten = ["Lisanne", "Paul", "Kees", "Marie", "Hilda" ]
+mooier_opgemaakt = f"De gasten {', '.join(gasten[:-1])} en {gasten[-1]} gaan samen uit eten."
+print(mooier_opgemaakt)
+
 gasten.remove("Marie")
+mooier_opgemaakt = f"De gasten {', '.join(gasten[:-1])} en {gasten[-1]} gaan samen uit eten."
+
+print(mooier_opgemaakt)
+
 index_kees = gasten.index("Kees")
 gasten.insert(index_kees + 1, ("George"))
+mooier_opgemaakt = f"De gasten {', '.join(gasten[:-1])} en {gasten[-1]} gaan samen uit eten."
 
-print(gasten)
+print(mooier_opgemaakt)
