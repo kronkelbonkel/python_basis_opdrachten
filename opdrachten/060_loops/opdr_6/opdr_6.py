e5de0d9a0050a2975c4c33844567835960fef7d5
kronkelbonkel <lisanneterhaar@gmail.com>
2024-02-12 11:47:58 +0100

    nou ik ben weer klaar voor vandaag joejoe

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -6,4 +6,25 @@
 
 # Hier start de for-loop
 
-my_list = []
+pizza_lijst = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+
+
+pizza_lijst.sort()
+print(pizza_lijst)
+
+
+pizza_lijst.append('snickers')
+print(pizza_lijst)
+
+
+pizza_lijst.remove('olivio')
+print(pizza_lijst)
+
+
+print(pizza_lijst[:3])
+
+
+print([pizza_lijst[len(pizza_lijst)//2]])
+
+
+print(pizza_lijst[-3:])
