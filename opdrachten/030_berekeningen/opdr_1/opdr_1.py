4dafe3908d444d58013e6a2b6749834985c0397a
kronkelbonkel <lisanneterhaar@gmail.com>
2024-02-12 09:49:34 +0100

    knikker

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,14 @@
 # Groep:
 
 # Hier komt je code...
+
+joe = 23
+bob = 15 
+fred = 22 
+
+hoi = joe+bob+fred 
+print(hoi)
+
+doei = hoi/3
+print(doei)
+
--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,22 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12 
+f = 10.4
+text = f"{c} graden Celcius is gelijk aan {f} graden Fahrenheit"
+print(text)
 
-print()
+f = 102
+c = 38.9
+text = f"{f} graden Fahrenheit is gelijk aan {c} graden Celcuis"
+print(text) 
+
+c = 62.2
+f = 144
+text = f"{c} graden Celcius is gelijk aan {f} graden Fahrenheit"
+print(text)
+
+f = 96
+c = 35.6
+text = f"{f} graden Fahrenheit is gelijk aan {c} graden Celcius"
+print(text)
